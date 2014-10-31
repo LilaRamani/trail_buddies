2ad8eae61182e37448688fc87c73a03c8380167b
LRamani <caroline.ramani@tufts.edu>
2014-10-30 23:13:02 -0400

    added geolocation and some css

--- map.js
+++ map.js
@@ -6,5 +6,19 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('map_canvas'),
     mapOptions);
+  MyLocation();
+}
+function MyLocation()
+{
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              myLat = position.coords.latitude;
+              myLng = position.coords.longitude;
+              renderMap();
+            });
+        }
+        else {
+          alert("Your Browser doesn't support geolocation");
+        }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
