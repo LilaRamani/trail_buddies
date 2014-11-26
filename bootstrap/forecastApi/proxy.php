0f217481dfd8cc4a4cdb8a5131cb1d5294816fae
elliot cobb <elliotcobb@elliots-MacBook-Pro.local>
2014-11-26 15:06:54 -0700

    added api key to proxy.php

--- bootstrap/forecastApi/proxy.php
+++ bootstrap/forecastApi/proxy.php
@@ -1,7 +1,7 @@
 <?php
 // File Name: proxy.php
 
-$api_key = 'API_KEY';
+$api_key = '200e4b0adde1dcf733e2eca4b88066ab';
 
 $API_ENDPOINT = 'https://api.forecast.io/forecast/';
 $url = $API_ENDPOINT . $api_key . '/';
