aff012a48f8b14a74133e89b3350c8054ef79b77
elliot cobb <elliotcobb@elliots-MacBook-Pro.local>
2014-11-28 18:10:13 -0700

    closed php file with ?>

--- bootstrap/forecastApi/proxy.php
+++ bootstrap/forecastApi/proxy.php
@@ -7,9 +7,9 @@ $API_ENDPOINT = 'https://api.forecast.io/forecast/';
 $url = $API_ENDPOINT . $api_key . '/';
 
 if(!isset($_GET['url'])) die();
+
 $url = $url . urldecode($_GET['url']);
 $url = file_get_contents($url);
 
-//print_r($url);
-
-echo $url;
+print_r($url);
+?>
